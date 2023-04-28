60b2a412f9f2675d4a44fda77eac6bfb7e55f143
KombinatorVit <slonik0775@gmail.com>
2023-04-28 10:16:33 +0300

    Initial commit

---
+++ src/utils/axios/index.ts
@@ -0,0 +1,24 @@
+// import axios from 'axios';
+//
+// export const instance = axios.create({
+//     baseURL:'https://api-v1.nzt-team.com/',
+//     timeout:1000,
+//     headers: {}
+// })
+//
+// export const instanceAuth = axios.create({
+//     baseURL: ``,
+//     timeout: 1000,
+//     headers: {
+//         'X-Custom-Header': 'foobar',
+//         Authorization: `Bearer ${token}`,
+//     },
+// })
+//
+// axios.get('https://api-v1.nzt-team.com/auth/login')
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.log(error.response.data);
+//     });
